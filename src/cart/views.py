565ec631ef713014c91373972d9e8d41bedc54df
John Doe <kth5604@gmail.com>
2019-04-24 05:55:31 +0900

    Style: 인덴트 수정

--- src/cart/views.py
+++ src/cart/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 
 def cart_list(request):
-    context = {
-        "count": 1,
-        "name": "kwon"
-    }
-    return render(request, 'cart/cart_list.html', context)
+    return render(
+        request,
+        'cart/cart_list.html',
+        {}
+    )
