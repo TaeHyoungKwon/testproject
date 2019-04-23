d0106ecdfc2d6f62c05fba9755696b70a571107b
John Doe <kth5604@gmail.com>
2019-04-24 05:43:05 +0900

    Fix: total 값 제대로 안뜨는 부분 get_total_price() 적용

--- src/cart/api/views.py
+++ src/cart/api/views.py
@@ -5,17 +5,16 @@ from ..models import Cart
 
 def cart_menu_list_api(request):
     cart, new_or_not = Cart.objects.new_or_get(request)
-    print(cart, new_or_not)
     menus_in_cart = [
         {
             "id": menu.id,
-            "name": menu.name,
+            "title": menu.title,
             "price": menu.price
         }
         for menu in cart.menus.all()
     ]
     data = {
         "menu": menus_in_cart,
-        "total": cart.total,
+        "total": cart.get_total_price(),
     }
     return JsonResponse(data)
