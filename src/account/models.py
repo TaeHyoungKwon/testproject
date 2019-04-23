ed76d633825dfa9e4abadf5a55412bfeef659dbc
John Doe <kth5604@gmail.com>
2019-04-24 02:47:19 +0900

    Feat: User 모델 매니저 추가

--- src/account/models.py
+++ src/account/models.py
@@ -1,8 +1,49 @@
 from django.db import models
 
 from django.contrib.auth.models import (
-    AbstractBaseUser
+    AbstractBaseUser,
+    BaseUserManager
 )
+
+
+class UserManager(BaseUserManager):
+    def create_user(self,
+                    email,
+                    password=None,
+                    is_active=True,
+                    is_staff=False,
+                    is_admin=False):
+        if not email:
+            raise ValueError("users must have an email address")
+        if not password:
+            raise ValueError("Users must have a password")
+
+        user_obj = self.model(
+            email=self.normalize_email(email)
+        )
+        user_obj.set_password(password)
+        user_obj.staff = is_staff
+        user_obj.admin = is_admin
+        user_obj.active = is_active
+        user_obj.save(using=self._db)
+        return user_obj
+
+    def create_staffuser(self, email, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+            is_staff=True
+        )
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+            is_staff=True,
+            is_admin=True
+        )
+        return user
 
 
 class User(AbstractBaseUser):
@@ -13,6 +54,8 @@ class User(AbstractBaseUser):
     staff = models.BooleanField(default=False)
     admin = models.BooleanField(default=False)
     timestampe = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
